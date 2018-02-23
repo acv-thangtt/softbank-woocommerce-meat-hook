2f48af7d86667f20dc6c18c0095d861cb4d5cc03
acv-thangtt <thangtt@atmarkcafe.org>
2018-02-23 17:54:35 +0900

    init

---
+++ softbank-woocommerce-meat-hook.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Softbank WooCommerce Meat Hook
+ * Plugin URI: http://fixbugtodie.com
+ * Description: Softbank WooCommerce Meat Hook
+ * Version: 1.0.0
+ * Author: fixbugtodie
+ * Author URI: http://fixbugtodie.com
+ * Requires at least: 4.4
+ * Tested up to: 4.9.4
+ *
+ * Text Domain: softbank-woocommerce-meat-hook
+ * Domain Path: /languages/
+ *
+ * @package FixBugToDie
+ * @category WooCommerce
+ * @author fixbugtodie
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
