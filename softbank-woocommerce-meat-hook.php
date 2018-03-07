6c14f82c695c9cfe91624e2011dbe36611b47d02
acv-thangtt <thangtt@atmarkcafe.org>
2018-03-07 11:42:54 +0900

    add hook change notice url

--- softbank-woocommerce-meat-hook.php
+++ softbank-woocommerce-meat-hook.php
@@ -11,6 +11,7 @@
  *
  * Text Domain: softbank-woocommerce-meat-hook
  * Domain Path: /languages/
+ * Docs: https://developer.sbpayment.jp
  *
  * @package FixBugToDie
  * @category WooCommerce
@@ -20,3 +21,21 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+
+add_filter( 'spgfw_request_get_pagecon_url', function () {
+	return 'http://stbfep.sps-system.com/MerchantPayResultRecieveSuccess.jsp';
+} );
+
+add_action( 'spgfw_after_get_response_by_method_redirect', function ( $order, $sb_key, $sb_val ) {
+	if ( SPGFW_COMPLETED === $sb_key ) {
+		global $spgfw;
+
+		$order_id    = $order->get_id();
+		$tracking_id = isset( $_REQUEST['res_tracking_id'] ) ? esc_attr( $_REQUEST['res_tracking_id'] ) : '';
+
+		if ( $tracking_id ) {
+			update_post_meta( $order_id, $spgfw->acf_tracking_id(), $tracking_id );
+		}
+	}
+}, 10, 3 );
